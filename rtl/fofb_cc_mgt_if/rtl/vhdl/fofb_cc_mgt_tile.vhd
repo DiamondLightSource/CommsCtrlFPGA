5b02174c5afcdd205a193794114d62b35f1ca33e
iu42 <iu42@e099a375-04f9-0310-9d5f-a741eaff62e1>
2012-07-25 13:34:59 +0000

    CommsCtrlFPGA : v3.00.1     * Set TX_DIF_CTRL attribute to 600 (600 mV) and the TXPREEMPHASIS attribute to 2 (25%) for Virtex2Pro RocketIO.
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/hardware/trunk/FPGA/CommsCtrlFPGA@80395 e099a375-04f9-0310-9d5f-a741eaff62e1

--- rtl/fofb_cc_mgt_if/rtl/vhdl/fofb_cc_mgt_tile.vhd
+++ rtl/fofb_cc_mgt_if/rtl/vhdl/fofb_cc_mgt_tile.vhd
@@ -263,7 +263,7 @@ MGT_INST : GT_CUSTOM
         TX_CRC_USE                  => TRUE,
         TX_DATA_WIDTH               => 2,
         TX_DIFF_CTRL                => 600,
-        TX_PREEMPHASIS              => 1,
+        TX_PREEMPHASIS              => 2,
         REF_CLK_V_SEL               => 1
 )
 port map (
